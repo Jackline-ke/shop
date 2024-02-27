95c51cc9f55905f9934cee4aed8fd9d08384c7ec
Jackline-ke <antoninahodhiambo@gmail.com>
2024-02-27 14:15:29 +0300

    added component folder

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
 import React from 'react'
-import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Footer from './component/Footer/Footer';
+import Navbar from './component/Navbar/Navbar';
+import Categories from './pages/Categories/Categories';
+import Home from './pages/Home/Home';
+import Product from './pages/Product/Product';
+
+//arrow function that will have contain the main components
+const Layout = () =>{
+  return(
+    <div className='app'>
+      <Navbar/>
+      <Outlet/>
+      <Footer/>
+    </div>
+  )
+}
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <span>Home</span>,
+    element: <Layout/>,
+    child: [
+      {
+        path:"/",
+        element: <Home/>
+      },
+      {
+        path:"/products/:id",
+        element: <Categories/>
+      },
+      {
+        path:"/product/:id",
+        element: <Product/>
+      },
+    ]
   },
-  {
-    path: "/category/:id",
-    element: <span>Categories</span>,
-  },
-  {
-    path: "/product/:id",
-    element: <span>Product</span>,
-  }
 ]);
 
 const App = () => {
