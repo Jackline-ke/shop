8b4dbba515c82793d621876cdae57ecb79c7c878
Jackline-ke <antoninahodhiambo@gmail.com>
2024-02-27 16:59:48 +0300

    dependencies

--- src/App.js
+++ src/App.js
@@ -1,49 +1,33 @@
-import React from 'react'
-import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Footer from './component/Footer/Footer';
 import Navbar from './component/Navbar/Navbar';
 import Categories from './pages/Categories/Categories';
 import Home from './pages/Home/Home';
 import Product from './pages/Product/Product';
 
-//arrow function that will have contain the main components
-const Layout = () =>{
-  return(
+const Layout = () => {
+  return (
     <div className='app'>
-      <Navbar/>
-      <Outlet/>
-      <Footer/>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products/:id" element={<Categories />} />
+        <Route path="/product/:id" element={<Product />} />
+      </Routes>
+      <Footer />
     </div>
-  )
-}
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout/>,
-    child: [
-      {
-        path:"/",
-        element: <Home/>
-      },
-      {
-        path:"/products/:id",
-        element: <Categories/>
-      },
-      {
-        path:"/product/:id",
-        element: <Product/>
-      },
-    ]
-  },
-]);
+  );
+};
 
 const App = () => {
   return (
-    <div>
-      <RouterProvider router={router} />
-    </div>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
